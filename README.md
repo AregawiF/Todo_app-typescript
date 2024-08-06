d896d629a23fba0be35b813b82cd1c0b60a13fd8
AregawiF <133565354+AregawiF@users.noreply.github.com>
2024-08-06 11:47:49 +0300

    added project files

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Todo_app-typescript
+
+## Introduction
+This app is designed to help users manage their to-do lists.
+
+## Features
+- Add tasks
+- Edit tasks
+- Delete tasks
+- Mark task as completed
+
+When the page loads initially you should see the following page.
+![Todo List App Screenshot](./screenshots/initial.png)
+
+## Adding a task
+To add a task just click on the input field and type in the task.
+![Todo List App Screenshot](./screenshots/typing.png)
+
+After that just click on 'add task' button. The task should be added and you should see the following page.
+![Todo List App Screenshot](./screenshots/added.png)
+
+## Marking a task as done
+To mark and unmark a task as done just click on the checkbox on the right.
+![Todo List App Screenshot](./screenshots/checking_as_done.png)
+
+## Editing a task
+To edit a task click on the 'edit' button.
+Then you will be prompted to add the new edited text.
+![Todo List App Screenshot](./screenshots/editing.png)
+
+After that click on the 'ok' button. The task should be edited with the new text and you should see the following page.
+![Todo List App Screenshot](./screenshots/edited.png)
+
+## Deleting a task
+To delete a task click on the 'delete' button. The task should be deleted and you should see the following page.
+![Todo List App Screenshot](./screenshots/delete.png)
+
+## To run the app
+Clone the repository by running the following command:
+```bash
+git clone https://github.com/AregawiF/Todo_app-typescript.git
+
+and open the index.html file
---
+++ dist/todo.js
@@ -0,0 +1,61 @@
+"use strict";
+const addButton = document.getElementById('addButton');
+const inputField = document.getElementById('inputField');
+const taskList = document.getElementById('taskList');
+let TodoList = [];
+function AddTask(newtask) {
+    let NewTodo = { task: '', done: false };
+    NewTodo.task = newtask.task;
+    NewTodo.done = newtask.done;
+    const newRow = document.createElement('div');
+    const newSpan = document.createElement('span');
+    const editButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
+    const buttonContainer = document.createElement("div");
+    const checkbox = document.createElement('input');
+    const listItem = document.createElement('li');
+    newSpan.textContent = NewTodo.task;
+    newSpan.id = 'textSpan';
+    checkbox.type = 'checkbox';
+    checkbox.id = 'checkbox';
+    editButton.textContent = 'Edit';
+    editButton.id = 'editButton';
+    deleteButton.textContent = 'Delete';
+    deleteButton.id = 'deleteButton';
+    buttonContainer.className = "button-container";
+    buttonContainer.appendChild(editButton);
+    buttonContainer.appendChild(deleteButton);
+    buttonContainer.appendChild(checkbox);
+    newRow.className = 'row';
+    newRow.appendChild(newSpan);
+    newRow.appendChild(buttonContainer);
+    listItem.appendChild(newRow);
+    taskList.appendChild(listItem);
+    inputField.value = '';
+    checkbox.onclick = () => {
+        if (NewTodo.done === false) {
+            newSpan.className = 'completed';
+            NewTodo.done = true;
+        }
+        else {
+            newSpan.classList.remove('completed');
+            NewTodo.done = false;
+        }
+    };
+    deleteButton.onclick = () => {
+        taskList.removeChild(listItem);
+    };
+    editButton.onclick = () => {
+        const editText = prompt('Edit task');
+        if (editText)
+            if (editText.trim()) {
+                newSpan.textContent = editText;
+            }
+    };
+}
+addButton.onclick = () => {
+    const taskText = inputField.value;
+    if (taskText) {
+        AddTask({ task: taskText, done: false });
+    }
+};
